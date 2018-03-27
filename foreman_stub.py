6c4e6f57b99cd8614062c9fd1012a3a529790487
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-03-27 19:25:53 +0200

    Refactor foreman stub

--- foreman_stub.py
+++ foreman_stub.py
@@ -1,67 +1,84 @@
 #!/usr/bin/env python
 
+"""Foreman API v2 stub server.
 
-# Create anything.foreman.yaml with this content ...
-#plugin: foreman
-#url: http://localhost:8080
-#user: ansible-reader
-#password: changeme
-#validate_certs: False
+Create anything.foreman.yaml with this contents::
 
-# Start this script
-# ./flaskapp.py
+    plugin: foreman
+    url: http://localhost:8080
+    user: ansible-reader
+    password: changeme
+    validate_certs: False
 
-# Run ansible-inventory to test
-# ansible-inventory -vvvv -i anything.foreman.yaml --list
+Start this script::
 
+    $ ./flaskapp.py
+
+Run ansible-inventory to test::
+
+    $ ansible-inventory -vvvv -i anything.foreman.yaml --list
+
+"""
 
 import glob
 import json
+import os
 
-from flask import Flask
-from flask import jsonify
-from flask import request
+from collections import defaultdict
+from urllib.parse import urlparse
+
+from flask import Flask, jsonify, request
+
+
+DEBUG = os.getenv('DEBUG', 'false').lower() not in ('false', '0', '', 'off')
+
+if DEBUG:
+    import pip
+    pip.main(['install', 'q', 'epdb'])
+
 app = Flask(__name__)
 
-PAGECACHE = {}
+PAGECACHE = defaultdict(dict)
+
 
 @app.before_first_request
 def build_pagecache():
+    """Read JSON fixtures into in-memory cache."""
     jfiles = glob.glob('fixtures/*.json')
     for jfile in jfiles:
         with open(jfile, 'r') as f:
-            jdata = json.loads(f.read())
-        key = (jdata['url'], jdata['page'])
-        PAGECACHE[key] = jdata
+            jdata = json.load(f)
+        key = urlparse(jdata['url']).path[7:]
+        subkey = jdata['page']
+        PAGECACHE[key][subkey] = jdata['data']
+
+
+def get_page_num():
+    """Return page number if present."""
+    pagenum = request.args.get('page')
+    if pagenum:
+        pagenum = int(pagenum)
+    return pagenum
 
 
 @app.route('/api/v2/hosts')
 @app.route('/api/v2/hosts/<hostid>')
 def get_hosts(hostid=None):
-    # page number?
-    pagenum = request.args.get('page')
-    if pagenum:
-        pagenum = int(pagenum)
+    """Render fixture contents from cache."""
+    pagenum = get_page_num()
 
-    keys = PAGECACHE.keys()
-    thiskey = None
-    for key in keys:
-        if key[-1] != pagenum:
-            continue
-        if hostid is None and key[0].endswith('/hosts'):
-            thiskey = key
-            break
-        elif hostid and key[0].endswith('/hosts/%s' % hostid):
-            thiskey = key
-            break
+    cache_key = '/hosts'
+    if hostid is not None:
+        cache_key = f'{cache_key}/{hostid}'
 
-    if not thiskey:
-        import epdb; epdb.st()
-
-    resp = PAGECACHE[thiskey]
-    #import epdb; epdb.st()
-    return jsonify(resp['data'])
+    try:
+        resp = PAGECACHE[cache_key][pagenum]
+    except KeyError:
+        if DEBUG:
+            import q; q/cache_key; q/pagenum; q/hostid
+            import epdb; epdb.st()
+    else:
+        return jsonify(resp)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False)
+__name__ == '__main__' and app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)), debug=DEBUG)
